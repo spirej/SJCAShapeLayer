3b3dd525c12cd0d3b227b118529683da1a4664d8
fushengjun <fushengjun@xiangshang360.com>
2017-02-17 10:01:47 +0800

    Initial Commit

---
+++ SJCAShapeLayer/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SJCAShapeLayer
+//
+//  Created by SPIREJ on 2017/2/17.
+//  Copyright © 2017年 SPIREJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
